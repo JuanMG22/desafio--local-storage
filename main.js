54a3330907b44cfb9c36cb4b001f46ef74aec21c
Juan Manuel González <juan.m-gonzalez@outlook.com>
2022-06-07 21:03:23 -0300

    ejercicio resuelto

--- main.js
+++ main.js
@@ -2,3 +2,35 @@ const formArticle = document.querySelector("#form")
 const clearBtn = document.querySelector("#btnClear")
 const showBtn = document.querySelector("#btnShowLocal")
 const posts = []
+
+class Post {
+    constructor(content) {
+        this.content = content
+    }
+}
+let postsLocalStorage = JSON.parse(localStorage.getItem('posts'))
+
+formArticle.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const article = document.querySelector('#textArticle').value
+        const newArticle = new Post(article)
+        posts.push(newArticle)
+        localStorage.setItem('posts', JSON.stringify(posts))
+        postsLocalStorage = JSON.parse(localStorage.getItem('posts'))
+
+    }
+)
+
+clearBtn.addEventListener('click', () => {
+    localStorage.clear()
+    postsLocalStorage = false
+    console.log('Se limpió el LocalStorage')
+})
+
+showBtn.addEventListener('click', () => {
+    if (postsLocalStorage) {
+        console.log(postsLocalStorage)
+    } else if(!postsLocalStorage) {
+        console.log('El localStorage está vacio')
+    }
+})
